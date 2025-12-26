8789216c66b2d4b1f9922287dd186e8f8f8d11f6
Gonzalo Patino <gpatinoc92@gmail.com>
2025-12-26 03:47:44 -0800

    Adjust vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gpatino.github.io/',
+  base: '/IoT_thermoStat_ecosystem/', // Add this line with your repo name
 })
